af8cbfb849f8aac676ebab9384da41a9476c8532
imed92 <imed.benamor.pro@gmail.com>
2024-11-26 02:41:10 +0100

    ok

---
+++ src/controllers/MainController.php
@@ -0,0 +1,19 @@
+<?php
+
+class MainController
+{
+    public function home()
+    {
+        echo "Welcome to the Home Page!";
+    }
+
+    public function about()
+    {
+        echo "This is the About Page.";
+    }
+
+    public function notFound()
+    {
+        echo "404 - Page Not Found!";
+    }
+}
